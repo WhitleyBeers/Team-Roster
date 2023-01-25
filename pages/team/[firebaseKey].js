ddacb85a33c5bdf29ebe1cd5d6ad0d8476d3d6c1
Whitley Beers <whitleybeers@gmail.com>
2023-01-25 17:33:36 -0600

    View team details shows team member cards on page

--- pages/team/[firebaseKey].js
+++ pages/team/[firebaseKey].js
@@ -1,7 +1,48 @@
-import React from 'react';
+import Head from 'next/head';
+import Link from 'next/link';
+import { useRouter } from 'next/router';
+import React, { useEffect, useState } from 'react';
+import { Button } from 'react-bootstrap';
+import { getMembersByTeam, getSingleTeam } from '../../api/teamData';
+import MemberCard from '../../components/Cards/MemberCards';
 
 export default function ViewTeam() {
+  const router = useRouter();
+  const { firebaseKey } = router.query;
+  const [members, setMembers] = useState([]);
+  const [team, setTeam] = useState([]);
+
+  const displayTeamMembers = () => {
+    getSingleTeam(firebaseKey).then(setTeam);
+    getMembersByTeam(firebaseKey).then(setMembers);
+  };
+
+  useEffect(() => {
+    displayTeamMembers();
+  }, []);
+
   return (
-    <div>[firebaseKey]</div>
+    <div className="text-center my-4">
+      <Head>
+        <title>Viewing {team.team_name}</title>
+      </Head>
+      <h1>{team.team_name}{team.public && (
+        <span>&#128275;</span>
+      )}
+      </h1>
+      <Link href={`/team/edit/${team.firebaseKey}`} passHref>
+        <Button className="custom-btn m-2">EDIT TEAM</Button>
+      </Link><hr />
+      <h3>Team Roster</h3>
+      <div className="d-flex flex-wrap">
+        {members.map((member) => (
+          <MemberCard
+            key={member.firebaseKey}
+            memberObj={member}
+            onUpdate={displayTeamMembers}
+          />
+        ))}
+      </div>
+    </div>
   );
 }
